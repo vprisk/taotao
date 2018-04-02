17e84e9e53c74b17552bf8fb6046e53a8b7d0958
Administrator <Administrator@computer-2018>
2018-04-02 10:44:37 +0800

    是否登录拦截器

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/PageController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/PageController.java
@@ -1,13 +1,18 @@
 package com.taotao.sso.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-	@RequestMapping("/page/{page}")
-	public String page(@PathVariable String page){
-		return page;
+	@RequestMapping("/page/register")
+	public String showRegister() {
+		return "register";
+	}
+	@RequestMapping("/page/login")
+	public String showLogin(String url, Model model) {
+		model.addAttribute("redirect", url);
+		return "login";
 	}
 }
